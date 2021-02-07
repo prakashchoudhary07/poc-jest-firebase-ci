add7e87dbfb0b9e163ce5533e59554fc47d2c4c5
Prakash <om.pcprakash@gmail.com>
2021-02-07 14:34:30 +0530

    test setup

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
   res.send("Ok");
 });
 
 app.listen(5000, () => {
-  console.log('started at port 5000');
+  console.log("started at port 5000");
 });
