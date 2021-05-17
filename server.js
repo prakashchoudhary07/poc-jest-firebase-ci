3e33ef806072242c44e231b1abe153f3fa6695c9
Prakash <om.pcprakash@gmail.com>
2021-05-17 08:52:27 +0530

    TEst

--- server.js
+++ server.js
@@ -4,15 +4,27 @@ const colortheif = require('colorthief');
 const app = express();
 
 app.get('/', async (req, res) => {
-  res.send('Ok');
+  try {
+    res.send('Ok');
+  } catch (err) {
+    res.status(500).json('Error while doing some thing', err);
+  }
 });
 
 app.get('/color', async (req, res) => {
-  const dominant = await colortheif.getColor(
-    'https://lokeshdhakar.com/projects/color-thief/image-2.4461c1c0.jpg'
-  );
-  console.log(dominant);
-  res.send(dominant);
+  try {
+    const dominant = await colortheif.getColor(
+      'https://lokeshdhakar.com/projects/color-thief/image-2.4461c1c0.jpg'
+    );
+    console.log(dominant);
+    res.send(dominant);
+  } catch (err) {
+    res.status(500).json('Error while doing some thing', err);
+  }
+});
+
+app.use(async (req, res) => {
+  res.status(500).json('NOT FOUND');
 });
 
 app.listen(5000, () => {
